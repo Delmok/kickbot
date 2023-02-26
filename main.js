f3d28581dd51567e8c098c4dd2f0ab87d2acf27d
gank <meatbagtv@gmail.com>
2023-02-26 12:21:53 -0500

    updated deps

--- main.js
+++ main.js
@@ -3,17 +3,14 @@ import events from 'events';
 
 export const chat = new events();
 
-
 const ws = new WebSocket('wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.4.0&flash=false');
 ws.on('error', console.error);
 
 export function connectToChat(channelID, chatroomsID) {
     
     ws.on('open', function open() {
-        let data = JSON.stringify({"event":"pusher:subscribe", "data":{"auth":"", "channel": `channel.${channelID}`}});
-        let data2 = JSON.stringify({"event":"pusher:subscribe","data":{"auth":"","channel":`chatrooms.${chatroomsID}`}});
-        ws.send(data);
-        ws.send(data2);
+        ws.send(JSON.stringify({"event":"pusher:subscribe", "data":{"auth":"", "channel": `channel.${channelID}`}}));
+        ws.send(JSON.stringify({"event":"pusher:subscribe","data":{"auth":"","channel":`chatrooms.${chatroomsID}`}}));
     });
 }
 
